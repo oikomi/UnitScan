a665ec3d6c512956a1c3bfb7734ec84b5ad89b88
Harold <miaohonghit@gmail.com>
2013-12-23 12:47:54 +0800

    init

--- us.py
+++ us.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 #主程序入口
 
 from net.coreSpider.coreSpider.spiders.coreSpider import main_spider
 
 def main():
     
-    #运行爬虫模块
+    #运行爬虫模块并启动扫描模块
     main_spider()
 
 if __name__ == '__main__':
